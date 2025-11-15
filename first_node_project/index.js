7edb0df51f7351467e11677098e6aa2c0c018287
kaur6252 <harshgurm@gmail.com>
2025-11-14 20:07:10 -0500

    worked on api request

--- first_node_project/index.js
+++ first_node_project/index.js
@@ -2,13 +2,69 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/name/', (req, res) => {
-  res.send('Hello, there I am changing this text right now!');
+const customers = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
+  { id: 3, name: 'Jim Beam' }  
+]
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Incoming request here`);
+  next();
+})
+
+function checkValidCustomerId(req, res, next) {
+  console.log('This is going to be executed for /customers/:id route only');
+  next();
+}
+
+app.get('/customers/', (req, res) => {
+  return res.send(customers);
 }); 
 
-// app.get('/customers', (req, res) => {
-//   res.send([{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }, { id: 3, name: 'Jim Beam' }]);
-// }); 
+
+
+app.get('/customers/:id', checkValidCustomerId, (req, res) => {
+  const cust_id = req.params.id;
+  const customer = customers.find(c => c.id === parseInt(cust_id));
+  if (customer) {
+    return res.send(customer);
+  } else {
+    return res.status(404).send('Customer not found');
+  }
+
+}); 
+
+app.post('/add_customers/', (req, res) => {
+  console.log('HERE', req.body.name);
+  const newCustomer = { id: customers.length + 1, name: req.body.name };
+  customers.push(newCustomer);
+  return res.status(201).send(newCustomer);
+})
+
+app.get('/users/', (req, res) => {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  fetch(url)
+    .then(response => response.json())
+    .then(data => res.send(data))
+    .catch(error => res.status(500).send('Error fetching users'));
+})
+
+
+app.put('/update_customer/:id', (req, res) => {
+  console.log('COming here')
+  const cust_id = req.params.id;
+  const customer = customers.find(c => c.id === parseInt(cust_id));
+  if (customer) {
+    customer.name = req.body.name || customer.name;    
+    return res.send(customer);
+  } else {
+    return res.status(404).send('Customer not found');
+  }
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
